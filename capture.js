2d3fa3d23556e0e58631b2303cb36d8c352793b0
Khue Vu <khuevu@gmail.com>
2012-08-09 00:09:53 +0800

    store access token to local storage

--- capture.js
+++ capture.js
@@ -1,9 +1,9 @@
 /* Utils function */
 function getViewableHeight() {
-    return Math.max(window.innerHeight, document.body.parentNode.scrollHeight, document.body.parentNode.clientHeight);
+	return Math.max(window.innerHeight, document.body.parentNode.scrollHeight, document.body.parentNode.clientHeight);
 }
 function getViewableWidth() {
-    return Math.max(window.innerWidth, document.body.parentNode.scrollWidth, document.body.parentNode.clientWidth);
+	return Math.max(window.innerWidth, document.body.parentNode.scrollWidth, document.body.parentNode.clientWidth);
 }
 
 //initialize currentTab Id: 
@@ -39,7 +39,7 @@ toolbox.style.width = '500px';
 capturePopup.appendChild(toolbox);
 //caption input box
 var captionContainer = document.createElement('div');
-captionContainer.style.width= '450px';
+captionContainer.style.width = '450px';
 var captionInput = document.createElement('textarea');
 captionInput.id = 'caption';
 captionInput.placeholder = 'Your caption message...'
@@ -61,106 +61,107 @@ shareFbButton.addEventListener('click', shareFacebook);
 document.body.appendChild(captureCanvas);
 document.body.appendChild(capturePopup);
 
-
 function CaptureView() {
-    this.popup = null;
-    this.imageCanvas = null;
-    this.show = function() {
-        this.popup.style.display = 'block'; 
-    }
-    this.hide = function() {
-        this.popup.style.display = 'none'; 
-    }
-    this.setImage = function(imageUrl, x, y, w, h) {
-        this.imageCanvas.width = w;
-        this.imageCanvas.height = h;
-        //set image 
-        var imgCtx = this.imageCanvas.getContext('2d');
-        var imageObj = new Image();
-        console.log('x ' + x + ', y ' + y + ', w ' + w + ', h' + h);
-        imageObj.onload = function() {
-            imgCtx.drawImage(imageObj, x, y, w, h, 0, 0, w, h);
-        }
-        imageObj.src = imageUrl;
-    }
-    this.initialize = function() {
-        this.popup = document.getElementById('captureView'); 
-        //set top and left position
-        var offsetTop = window.innerHeight / 2 - parseInt(this.popup.style.height) / 2 + window.scrollY;
-        offsetTop = offsetTop > 0 ? offsetTop : 0;
-        var offsetLeft = window.innerWidth / 2 - parseInt(this.popup.style.width) / 2 + window.scrollX;
-        offsetLeft = offsetLeft > 0 ? offsetLeft : 0; 
-        console.log('offset top ' + offsetTop + ' - offset left ' + offsetLeft);
-        this.popup.style.top = offsetTop + 'px'; 
-        this.popup.style.left = offsetLeft + 'px';
-        //set image display canvas
-        this.imageCanvas = this.popup.querySelector("#imageDisplay");
-    }
-    this.initialize();
+	this.popup = null;
+	this.imageCanvas = null;
+	this.show = function() {
+		this.popup.style.display = 'block';
+	}
+	this.hide = function() {
+		this.popup.style.display = 'none';
+	}
+	this.setImage = function(imageUrl, x, y, w, h) {
+		this.imageCanvas.width = w;
+		this.imageCanvas.height = h;
+		//set image 
+		var imgCtx = this.imageCanvas.getContext('2d');
+		var imageObj = new Image();
+		console.log('x ' + x + ', y ' + y + ', w ' + w + ', h' + h);
+		imageObj.onload = function() {
+			imgCtx.drawImage(imageObj, x, y, w, h, 0, 0, w, h);
+		}
+		imageObj.src = imageUrl;
+	}
+	this.initialize = function() {
+		this.popup = document.getElementById('captureView');
+		//set top and left position
+		var offsetTop = window.innerHeight / 2 - parseInt(this.popup.style.height) / 2 + window.scrollY;
+		offsetTop = offsetTop > 0 ? offsetTop: 0;
+		var offsetLeft = window.innerWidth / 2 - parseInt(this.popup.style.width) / 2 + window.scrollX;
+		offsetLeft = offsetLeft > 0 ? offsetLeft: 0;
+		console.log('offset top ' + offsetTop + ' - offset left ' + offsetLeft);
+		this.popup.style.top = offsetTop + 'px';
+		this.popup.style.left = offsetLeft + 'px';
+		//set image display canvas
+		this.imageCanvas = this.popup.querySelector("#imageDisplay");
+	}
+	this.initialize();
 }
 
 function CaptureCanvasController() {
-    this.canvas = null;
-    this.startPoint = null;
-    this.endPoint = null;
-    this.mousePressed = false;
-    //handle mouse pressed when user start selection
-    this.startSelection = function(startPoint) {
-        this.mousePressed = true;      
-        this.startPoint = startPoint; 
-    };
-    //handle mouse drag
-    this.updateSelection = function(currentPoint) {
-        if (!this.mousePressed)
-            return;
-        //clear old selection drawing: 
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        //draw the new selection
-        var width = currentPoint.x - this.startPoint.x;
-        var height = currentPoint.y - this.startPoint.y;
-        //console.log("start: " + this.startPoint.x + " - " + this.startPoint.y + " width, height: " + width + " - " + height);
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        //highlight selection
-        this.ctx.clearRect(this.startPoint.x, this.startPoint.y, width, height);
-        this.ctx.strokeRect(this.startPoint.x, this.startPoint.y, width, height);
-    }; 
-    //handle mouse released
-    this.endSelection = function(endPoint) {
-        this.mousePressed = false;
-        this.endPoint = endPoint; 
-    };
+	this.canvas = null;
+	this.startPoint = null;
+	this.endPoint = null;
+	this.mousePressed = false;
+	//handle mouse pressed when user start selection
+	this.startSelection = function(startPoint) {
+		this.mousePressed = true;
+		this.startPoint = startPoint;
+	};
+	//handle mouse drag
+	this.updateSelection = function(currentPoint) {
+		if (!this.mousePressed) return;
+		//clear old selection drawing: 
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		//draw the new selection
+		var width = currentPoint.x - this.startPoint.x;
+		var height = currentPoint.y - this.startPoint.y;
+		//console.log("start: " + this.startPoint.x + " - " + this.startPoint.y + " width, height: " + width + " - " + height);
+		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+		//highlight selection
+		this.ctx.clearRect(this.startPoint.x, this.startPoint.y, width, height);
+		this.ctx.strokeRect(this.startPoint.x, this.startPoint.y, width, height);
+	};
+	//handle mouse released
+	this.endSelection = function(endPoint) {
+		this.mousePressed = false;
+		this.endPoint = endPoint;
+	};
 
-    this.initialize = function() {
-        this.canvas = document.getElementById('captureCanvas');
-        this.ctx = this.canvas.getContext('2d');
-        this.ctx.strokeStyle = 'black';
-        this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        //disable mouse scrolling
-        document.body.style.overflow = 'hidden';
-    }
+	this.initialize = function() {
+		this.canvas = document.getElementById('captureCanvas');
+		this.ctx = this.canvas.getContext('2d');
+		this.ctx.strokeStyle = 'black';
+		this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+		//disable mouse scrolling
+		document.body.style.overflow = 'hidden';
+	}
 
-    this.captureSelection = function() {
-        //use window.scrollX, window.scrollY 
-        var x = this.startPoint.x - window.scrollX;
-        var y = this.startPoint.y - window.scrollY;
-        var width = this.endPoint.x - this.startPoint.x;
-        var height = this.endPoint.y - this.startPoint.y;
-        //send start and end points coo)dinate to background process
-        chrome.extension.sendRequest({action: 'capture'}, function(response) {
-                captureView.setImage(response.image, x, y, width, height);
-                captureView.show();
-            }); 
-    };
+	this.captureSelection = function() {
+		//use window.scrollX, window.scrollY 
+		var x = this.startPoint.x - window.scrollX;
+		var y = this.startPoint.y - window.scrollY;
+		var width = this.endPoint.x - this.startPoint.x;
+		var height = this.endPoint.y - this.startPoint.y;
+		//send start and end points coo)dinate to background process
+		chrome.extension.sendRequest({
+			action: 'capture'
+		},
+		function(response) {
+			captureView.setImage(response.image, x, y, width, height);
+			captureView.show();
+		});
+	};
 
-    this.exit = function() {
-        document.body.removeChild(this.canvas);
-        document.body.removeChild(captureView.popup);
-        //enable scrolling
-        document.body.style.overflow = 'scroll';
-    }
+	this.exit = function() {
+		document.body.removeChild(this.canvas);
+		document.body.removeChild(captureView.popup);
+		//enable scrolling
+		document.body.style.overflow = 'scroll';
+	}
 
-    this.initialize();
+	this.initialize();
 }
 
 //initialize the controller
@@ -168,103 +169,116 @@ var controller = new CaptureCanvasController();
 var captureView = new CaptureView();
 //bind event handler 
 captureCanvas.onmousedown = function() {
-    controller.startSelection({x: event.offsetX, y: event.offsetY}); 
+	controller.startSelection({
+		x: event.offsetX,
+		y: event.offsetY
+	});
 }
 captureCanvas.onmouseup = function() {
-    controller.endSelection({x: event.offsetX, y: event.offsetY});
-} 
+	controller.endSelection({
+		x: event.offsetX,
+		y: event.offsetY
+	});
+}
 captureCanvas.onmousemove = function() {
-    controller.updateSelection({x: event.offsetX, y: event.offsetY});
+	controller.updateSelection({
+		x: event.offsetX,
+		y: event.offsetY
+	});
 }
 document.onkeydown = function() {
-    console.log('event ' + event);
-    var keyId = event.keyCode; 
-    console.log('Key ID ' + keyId);
-    switch(keyId) {
-        case 13: controller.captureSelection();
-            break;
-        case 27: controller.exit();
-            break;
-    }
+	console.log('event ' + event);
+	var keyId = event.keyCode;
+	console.log('Key ID ' + keyId);
+	switch (keyId) {
+	case 13:
+		controller.captureSelection();
+		break;
+	case 27:
+		controller.exit();
+		break;
+	}
 }
 //
 //simulate FireFox sendAsBinary for Chrome
 if (XMLHttpRequest.prototype.sendAsBinary === undefined) {
-    XMLHttpRequest.prototype.sendAsBinary = function(string) {
-        var bytes = Array.prototype.map.call(string, function(c) {
-                return c.charCodeAt(0) & 0xff; 
-            }); 
-        this.send(new Uint8Array(bytes).buffer);
-    }
-}   
+	XMLHttpRequest.prototype.sendAsBinary = function(string) {
+		var bytes = Array.prototype.map.call(string, function(c) {
+			return c.charCodeAt(0) & 0xff;
+		});
+		this.send(new Uint8Array(bytes).buffer);
+	}
+}
 // -------------------- FACEBOOK
 // ------------------------------------------------//
 function shareFacebook() {
-    debugger;
-    //request to authorization page
-    var authorizeUrl = 'https://www.facebook.com/dialog/oauth?client_id=425303964177321&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=publish_stream&response_type=token';
-    //chrome.extension.onRequest.addListener
-    //send request to background  
-    var popupWindow = null;
-    chrome.extension.sendRequest({'action': 'activate'}, function(response) {
-            //close the window.  
-            //popupWindow.close();
-            fbAccessToken = response.accessToken; 
-            console.log('Access Token ' + fbAccessToken);
-            console.log('Expire IN ' + response.expireIn);
-            //TODO: save accessToken to cookie
-            console.log(captureView.imageCanvas);
-            //var imageData = captureView.imageCanvas.toDataURL();
-            captureView.imageCanvas.toBlob(function(blob) {
-                    var fileReader = new FileReader(); 
-                    fileReader.onloadend = function(evt) {
-                        console.log('function happen here');
-                        if (event.target.readyState == FileReader.DONE) {
-                            uploadFacebookPhoto(evt.target.result, 'TempCaption', fbAccessToken); 
-                        
-                        } 
-                    }
-                    fileReader.readAsBinaryString(blob); 
-                }, "image/png"); 
+	debugger;
+	//request to authorization page
+	var authorizeUrl = 'https://www.facebook.com/dialog/oauth?client_id=425303964177321&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=publish_stream&response_type=token';
+	//send request to background  
+	var popupWindow = null;
+    chrome.storage.local.get('accessToken', function(data){
+            var accessToken = data.accessToken;         
+            console.log(accessToken);
+            if (accessToken) {
+                getDataAndUploadToFacebook(accessToken);  
+            } else {
+                 chrome.extension.sendRequest({
+                    'action': 'activate'
+                },
+                function(response) {
+                    //close the window.  
+                    //popupWindow.close();
+                    chrome.storage.local.set({'accessToken': response.accessToken});
+                    getDataAndUploadToFacebook(response.accessToken);
+                });
+                //setTimeout(200, function() {console.log('time out');})
+                popupWindow = window.open(authorizeUrl, "Facebook Permission", "left=" + ((window.screenX || window.screenLeft) + 10) + ",top=" + ((window.screenY || window.screenTop) + 10) + ",height=420px,width=550px,resizable=1,alwaysRaised=1");
+            } 
         });
-    
-    //setTimeout(200, function() {console.log('time out');})
-    popupWindow = window.open(authorizeUrl,"Facebook Permission","left="+((window.screenX||window.screenLeft)+10)+",top="+((window.screenY||window.screenTop)+10)+",height=420px,width=550px,resizable=1,alwaysRaised=1");  
-    //popupWindow = window.open(authorizeUrl);
-    console.log(popupWindow);
-}
+	
+	}
 
 function prepareMIMEMessage(binData, message, accessToken) {
-    var boundary = '-------';
-    var newLine = '\r\n';
-    var blankLine = '\r\n\r\n';
-    //binaryData
-    var data = newLine + boundary + newLine + "Content-Disposition: form-data; name=\"source\"; filename=\"capture.png\"" + newLine + "Content-Type: image/png" + blankLine + binData;
-    //form data: message
-    data = data + newLine + boundary + newLine + "Content-Disposition: form-data; name=\"message\"" + blankLine + message ;
-    //end
-    data = data + newLine + boundary + '--' + newLine;
-    return data;
-    
-}
+	var boundary = '-------';
+	var newLine = '\r\n';
+	var blankLine = '\r\n\r\n';
+	//binaryData
+	var data = newLine + boundary + newLine + "Content-Disposition: form-data; name=\"source\"; filename=\"capture.png\"" + newLine + "Content-Type: image/png" + blankLine + binData;
+	//form data: message
+	data = data + newLine + boundary + newLine + "Content-Disposition: form-data; name=\"message\"" + blankLine + message;
+	//end
+	data = data + newLine + boundary + '--' + newLine;
+	return data;
 
+}
+function getDataAndUploadToFacebook(fbAccessToken) {
+    var caption = captionInput.innerText;
+	captureView.imageCanvas.toBlob(function(blob) {
+		var fileReader = new FileReader();
+		fileReader.onloadend = function(evt) {
+			if (event.target.readyState == FileReader.DONE) {
+				uploadFacebookPhoto(evt.target.result, caption, fbAccessToken);
+			}
+		}
+		fileReader.readAsBinaryString(blob);
+	},
+	"image/png");
+}
 
 function uploadFacebookPhoto(imageData, caption, accessToken) {
-    
-    var photoUrl = 'https://graph.facebook.com/651244951/photos?access_token=' + accessToken;
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", photoUrl, true);
-    xhr.setRequestHeader("Content-type", "multipart/form-data; boundary=-----")
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            console.log('successfully post the message'); 
-        } 
-    };
-    //TODO: form post image data
-    var data = prepareMIMEMessage(imageData, caption, accessToken);
-    console.log(data);
-    xhr.sendAsBinary(data);
+
+	var photoUrl = 'https://graph.facebook.com/651244951/photos?access_token=' + accessToken;
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", photoUrl, true);
+	xhr.setRequestHeader("Content-type", "multipart/form-data; boundary=-----")
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			console.log('successfully post the message');
+		}
+	};
+	var data = prepareMIMEMessage(imageData, caption, accessToken);
+	xhr.sendAsBinary(data);
 
 }
-
 
